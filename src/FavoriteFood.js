b20ddf2a42fd72010f37be51c19505b5dee4af1d
Stacey Fernandes <stacey.fernandes@rea-group.com>
2018-04-07 20:50:55 +1000

    Create React app

---
+++ src/FavoriteFood.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class FavoriteFood extends Component {
+  constructor() {
+    super();
+    this.state = {
+      label: "YES I love burgers",
+      favorite: true,
+    }
+
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onClickHandler() {
+    if (this.state.favorite) {
+      this.setState({
+        label: "I hate burgers but I love salad",
+        favorite: false,
+      })
+    } else { this.setState({
+        label: "YES I love burgers",
+        favorite: true,
+    })}
+  }
+
+  render() {
+    return(
+      <div>Are burgers your favorite food?
+        <br />
+          <button onClick= { this.onClickHandler } />
+          {this.state.label}
+      </div>
+
+    )
+  }
+}
+
+export default FavoriteFood;
