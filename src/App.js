dec320f44a2dbde2155c0ff57bdcb5b14ce2e8df
Stacey Fernandes <stacey.fernandes@rea-group.com>
2018-04-07 21:30:17 +1000

    Add new icon,edit app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './burgerIcon.png';
 import FavoriteFood from './FavoriteFood';
 import './App.css';
 
@@ -9,11 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">What food do you like?</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <FavoriteFood />
       </div>
     );
