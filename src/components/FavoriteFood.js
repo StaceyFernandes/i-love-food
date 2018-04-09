6442ea8fbbe112b3dfe0d25e3acd8a78460d1666
Stacey Fernandes <stacey.fernandes@rea-group.com>
2018-04-09 19:26:11 +1000

    WIP Image

--- src/components/FavoriteFood.js
+++ src/components/FavoriteFood.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Button = styled.div`
+const Button = styled.button`
   height: 25px;
   widht: 15px;
   border-radius: 2px;
@@ -9,14 +9,17 @@ const Button = styled.div`
   border: 1px solid black;
   background-color: grey;
 `;
+const Img = styled.div`
+`;
 
 class FavoriteFood extends Component {
   constructor() {
     super();
     this.state = {
-      labelYesNo: "YES",
-      label: "I love burgers",
+      labelYesNo: "NEGATIVE",
+      label: "I love burgers too",
       favorite: true,
+      image: '../images/wineTable.jpg',
     }
     this.onClickHandler = this.onClickHandler.bind(this);
   }
@@ -27,20 +30,26 @@ class FavoriteFood extends Component {
          label: "I hate burgers but I love salad",
          favorite: false,
          labelYesNo: "YESSS",
+         image: "../images/wineTable.jpg",
         })) :
        (this.setState({
-         label: "I love burgers",
+         label: "I love burgers too",
          favorite: true,
          labelYesNo: "NEGATIVE",
+         image: "../images/wineTable.jpg"
         }))
  }
 
   render() {
+  //  const image = {this.state.image}
     return(
       <div>Are burgers your favorite food?
         <br />
-          <Button onClick= { this.onClickHandler }>{this.state.labelYesNo}</Button>
-          {this.state.label}
+        <Button onClick= { this.onClickHandler }>{this.state.labelYesNo}</Button>
+        <br />
+        {this.state.label}
+        <img src={require('../images/wineTable.jpg')} alt="hello" />
+
       </div>
     )
   }
