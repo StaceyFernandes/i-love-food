049fdfa2fb578bd3ab5229afe929dc8564b2c803
Stacey Fernandes <stacey.fernandes@rea-group.com>
2018-04-28 12:55:39 +1000

    Add test

---
+++ src/components/FavoriteFood.test.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+// import { shallow } from 'enzyme';
+// import FavoriteFood from './FavoriteFood';
+//
+// describe('FavoriteFood', () => {
+//   let toggleComponent;
+//
+//   toggleComponent = shallow(<Toggle defaultChecked={ true } onClickHandler= { this.onClickHandler} }/>);
+//
+//   it('state is false when clicked', () => {
+//     expect(toggleComponent.called).toBe(true);
+//   });
+// });
+//
+// export default FavoriteFood;
